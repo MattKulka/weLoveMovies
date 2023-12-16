d6b9ee3c155e81f47f27a33718e9edfe7cabd996
mattkulka <matthewkulka@outlook.com>
2023-12-15 19:02:58 -0500

    fixed errors

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,47 @@
-const path = require("path");
+// Update with your config settings.
 
-require("dotenv").config();
-
-const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-} = process.env;
-
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
+
   development: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
-
-  test: {
-    client: "sqlite3",
+    client: 'postgresql',
     connection: {
-      filename: ":memory:",
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
-  },
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
